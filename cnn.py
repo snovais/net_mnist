2fb1d7e51d385b835a54ace1faa67d6212a1ec3e
Sergio Novais <sergio.novais90@gmail.com>
2021-04-03 20:00:09 -0300

    cnn corrigida 1

--- cnn.py
+++ cnn.py
@@ -17,14 +17,12 @@ import processing as pr
 
 # load data and prepare
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+
+x_train, y_train = pr.canny_filter(x_train, y_train)
+x_test, y_test = pr.canny_filter(x_test, y_test)
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
 
-x_train, y_train = pr.canny_filter(x_train, y_train)
-print(x_train.shape)
-x_test, y_test = pr.canny_filter(x_test, y_test)
-
-"""
 # Add a channels dimension
 x_train = x_train[..., tf.newaxis].astype("float32")
 x_test = x_test[..., tf.newaxis].astype("float32")
@@ -39,12 +37,12 @@ test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(32)
 class MyModel(Model):
   def __init__(self):
     super(MyModel, self).__init__()
-    self.conv1 = Conv2D(32, 3, activation='relu')
+    self.conv1 = Conv2D(32, 5, activation='relu')
     self.pool1 = MaxPool2D(pool_size = (16, 16))
-    self.conv2 = Conv2D(32, 3, activation='relu')
-    self.pool1 = MaxPool2D(pool_size = (9, 9))
+    self.conv2 = Conv2D(32, 5, activation='relu')
+    self.pool1 = MaxPool2D(pool_size = (16, 16))
     self.flatten = Flatten()
-    self.d1 = Dense(1024, activation='relu')
+    self.d1 = Dense(12800, activation='relu')
     self.dr1 = Dropout(0.3)
     self.d2 = Dense(1024, activation='relu')
     self.dr2 = Dropout(0.3)
@@ -88,7 +86,7 @@ test_loss = tf.keras.metrics.Mean(name='test_loss')
 test_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='test_accuracy')
 
 
-@tf.function(jit_compile=True)
+@tf.function
 def train_step(images, labels):
   with tf.GradientTape() as tape:
     # training=True is only needed if there are layers with different
@@ -102,7 +100,7 @@ def train_step(images, labels):
   train_accuracy(labels, predictions)
   
 
-@tf.function(jit_compile=True)
+@tf.function
 def test_step(images, labels):
   # training=False is only needed if there are layers with different
   # behavior during training versus inference (e.g. Dropout).
@@ -136,5 +134,5 @@ for epoch in range(EPOCHS):
     f'Test Accuracy: {test_accuracy.result() * 100}'
   )
 
-"""
+
   
